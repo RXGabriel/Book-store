5467c8ea11fd8bfa2c942bc67591bc49730c8cd2
RXGabriel <gabriel2005rf@gmail.com>
2024-10-31 17:44:17 -0300

    feat: add Express server with DataBase connection, CORS, and basic API endpoint

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const connectDB = require("./utils/db");
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Servidor rodando na porta ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Erro ao conectar com o banco de dados", error.message);
+  });
+
+app.use(
+  cors({
+    origin: process.env.ORIGIN || "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Api rodando");
+});
