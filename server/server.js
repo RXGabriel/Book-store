afb2d68eaea10c17e8c33cadc9381ad86958402b
RXGabriel <gabriel2005rf@gmail.com>
2024-11-01 11:14:12 -0300

    feat: add book routes into the server application

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./utils/db");
 const cors = require("cors");
+const bookRoutes = require("./routes/book-routes");
 
 require("dotenv").config();
 const app = express();
@@ -32,6 +33,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use("/api/books", bookRoutes);
 
 app.get("/", (req, res) => {
   res.send("Api rodando");
