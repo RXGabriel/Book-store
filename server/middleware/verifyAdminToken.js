f51993e175c5e48361bf55a021ca8ce40ba7c2e0
RXGabriel <gabriel2005rf@gmail.com>
2024-11-01 10:15:58 -0300

    feat: add admin token verification middleware

---
+++ server/middleware/verifyAdminToken.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = process.env.JWT_SECRET_KEY;
+
+const verifyAdminToken = (req, res, next) => {
+  const token = req.header["authorization"]?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({ message: "Acesso negado" });
+  }
+
+  jwt.verify(token, JWT_SECRET, (error, user) => {
+    if (error) {
+      return res.status(403).json({ message: "Credenciais inválidas" });
+    }
+    req.user = user;
+    next();
+  });
+};
+
+module.exports = verifyAdminToken;
