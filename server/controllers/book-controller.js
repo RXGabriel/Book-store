3ebfb80d95068678ec8c852476ab35d12c09ff78
RXGabriel <gabriel2005rf@gmail.com>
2024-11-01 10:43:32 -0300

    feat: add getSingleBook controller to retrieve a specific book

--- server/controllers/book-controller.js
+++ server/controllers/book-controller.js
@@ -23,4 +23,18 @@ const getAllBooks = async (req, res) => {
   }
 };
 
-module.exports = { addBook, getAllBooks };
+const getSingleBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+
+    if (!book) res.status(404).send({ message: "Livro não encontrado" });
+
+    res.status(200).send(book);
+  } catch (error) {
+    console.log("Erro ao obter livro:", error.message);
+    res.status(500).send({ message: "Erro ao obter o livro" });
+  }
+};
+
+module.exports = { addBook, getAllBooks, getSingleBook };
