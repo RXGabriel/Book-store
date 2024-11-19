cfeeb1be7bfa843d0852cb56c82defb618b4ef32
RXGabriel <gabriel2005rf@gmail.com>
2024-11-19 10:42:30 -0300

    feat: integrate booksApi into Redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice";
+import booksApi from "./api/booksApi";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    [booksApi.reducerPath]: booksApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(booksApi.middleware),
 });
