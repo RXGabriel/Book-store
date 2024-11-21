b18536775ba05a6b68fbd029d2148e56f08cded7
RXGabriel <gabriel2005rf@gmail.com>
2024-11-21 09:58:34 -0300

    feat: integrate orders API into Redux store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from "./features/cartSlice";
 import booksApi from "./api/booksApi";
+import ordersApi from "./api/ordersApi";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
     [booksApi.reducerPath]: booksApi.reducer,
+    [ordersApi.reducerPath]: ordersApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(booksApi.middleware),
+    getDefaultMiddleware().concat(booksApi.middleware, ordersApi.middleware),
 });
